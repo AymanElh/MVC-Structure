20833ec530a34e0cc7fa884792a1b2c7755f9d21
AymanElh <elhamioui1964@gmail.com>
2025-02-05 20:50:30 +0100

    fix the problem about the articles page

--- app/Core/View.php
+++ app/Core/View.php
@@ -1,14 +1,29 @@
 <?php
 
 namespace App\Core;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 class View
 {
+    public string $title = '';
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new FilesystemLoader(Application::$ROOT_PATH . '/app/views');
+        $this->twig = new Environment($loader);
+    }
+
     public function renderView($view, $params = [])
     {
-        $veiwContent = $this->renderOnlyView($view, $params);
-        $layoutContent = $this->layoutContent();
-        return str_replace('{{content}}', $veiwContent, $layoutContent);
+        if ($this->isTwigView($view)) {
+            return $this->renderTwigView($view, $params);
+        } else {
+            $veiwContent = $this->renderOnlyView($view, $params);
+            $layoutContent = $this->layoutContent();
+            return str_replace('{{content}}', $veiwContent, $layoutContent);
+        }
     }
 
     public function renderContent($veiwContent)
@@ -19,31 +34,34 @@ class View
 
     private function layoutContent()
     {
+        $layout = 'main';
+        if(isset(Application::$app->controller)) {
+            $layout = Application::$app->controller->layout;
+        }
+
         ob_start();
-        include_once Application::$ROOT_PATH . "/app/views/layouts/main.php";
+        include_once Application::$ROOT_PATH . "/app/views/layouts/$layout.php";
         return ob_get_clean();
     }
 
     private function renderOnlyView($view, $params)
     {
-        foreach ($params as $key => $value) {
+        foreach($params as $key => $value) {
             $$key = $value;
         }
-
-        $frontViewPath = Application::$ROOT_PATH . "/app/views/front/$view.html.twig";
-        dump($frontViewPath);
-        $backViewPath = Application::$ROOT_PATH . "/app/views/back/$view.html.twig";
-
-        if (file_exists($frontViewPath)) {
-            $viewPath = $frontViewPath;
-        } elseif (file_exists($backViewPath)) {
-            $viewPath = $backViewPath;
-        } else {
-            throw new \Exception("View file '$view' not found in front or back.");
-        }
-
         ob_start();
-        include_once $viewPath;
+        include_once Application::$ROOT_PATH . "/app/views/$view.php";
         return ob_get_clean();
+    }
+
+    private function renderTwigView($view, $params)
+    {
+        echo $this->twig->render("front/$view.html.twig", $params);
+    }
+
+    private function isTwigView($view)
+    {
+        return file_exists(Application::$ROOT_PATH . "/app/views/front/$view.html.twig") ||
+            file_exists(Application::$ROOT_PATH . "/app/views/back/$view.html.twig");
     }
 }
